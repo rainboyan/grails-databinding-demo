e04d64e13475d9e1c5a1d6e15d66f86d5160f4d3
Michael Yan <rain@rainboyan.com>
2022-07-05 10:50:20 +0800

    Add domain post and generate all

---
+++ grails-app/domain/org/grails/demo/Post.groovy
@@ -0,0 +1,13 @@
+package org.grails.demo
+
+import java.time.LocalDate
+import java.time.ZonedDateTime
+
+class Post {
+    String title
+    LocalDate day
+    ZonedDateTime createdDate
+
+    static constraints = {
+    }
+}
